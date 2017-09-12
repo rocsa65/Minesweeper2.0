482099f1b422e402f241949ac9083b6023410c97
Roxana Sendrea <rocsa65@yahoo.com>
2017-09-12 09:41:56 +0100

    Dinamically generate player board

--- src/minesweeper.js
+++ src/minesweeper.js
@@ -1,19 +1,45 @@
-const printBoard = board => {
-    console.log(`Current board:`);
-    console.log(board[0].join(' | '));
-    console.log(board[1].join(' | '));
-    console.log(board[2].join(' | '));
+const generatePlayerBoard = (numberOfRows, numberOfColumns) => {
+    let board = [];
+    for(let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for(j = 0; j < numberOfColumns; j++) {
+            row.push(' ');
+        }
+        board.push(row);
+    }
+    return board;
 };
 
-let board = [
-    [' ', ' ', ' '],
-    [' ', ' ', ' '],
-    [' ', ' ', ' ']
-];
+const generateBombBoard = (numberOfRows, numberOfColumns, numberOfBombs) => {
+    let board = [];
+    for(let i = 0; i < numberOfRows; i++) {
+        let row = [];
+        for(j = 0; j < numberOfColumns; j++) {
+            row.push(null);
+        }
+        board.push(row);
+    }
 
-console.log(printBoard(board));
+    let numberOfBombsPlaced = 0;
+    while(numberOfBombsPlaced < numberOfBombs) {
+        let randomRowIndex = Math.floor(Math.random() * numberOfRows);
+        let randomColumnIndex = Math.floor(Math.random() * numberOfColumns);
+        board[randomRowIndex][randomColumnIndex] = 'B';
+        numberOfBombsPlaced++;
+    }
 
-board[0][1] = '1';
-board[2][2] = 'B';
+        return board;
+};
 
-console.log(printBoard(board));
+const printBoard = board => {
+    console.log(board.map(row => row.join(' | ')).join('\n'));
+};
+
+let playerBoard = generatePlayerBoard(3, 4);
+let bombBoard = generateBombBoard(3, 4, 5);
+
+console.log('Player Board:');
+printBoard(playerBoard);
+
+console.log('Bomb Board:');
+printBoard(bombBoard);
